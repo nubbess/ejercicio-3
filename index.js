7ed7c79168bfb061b72555b3acfb6adfede2eb2e
nubbess <julianms999@gmail.com>
2022-10-31 01:44:48 -0300

    ejercicio 3

---
+++ index.js
@@ -0,0 +1,15 @@
+var meses = ["enero", 
+"febrero", 
+"marzo", 
+"abril", 
+"mayo", 
+"junio", 
+"julio", 
+"agosto", 
+"septiembre", 
+"octubre", 
+"noviembre", 
+"diciembre"];
+meses.forEach(element => {
+    console.log(element)
+});
